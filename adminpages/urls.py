e7dc6914da4fd554973a69b13b9f68906838c72e
joelscariajustine123 <joelscaria2002@gmail.com>
2025-07-07 23:32:01 +0530

    attach html to django

--- adminpages/urls.py
+++ adminpages/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('',home,name='admin_home'),
+    path('', views.home, name='home'),  # Index page
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('booking/', views.booking, name='booking'),
+    path('payment/', views.payment, name='payment'),
+    path('report/', views.report, name='report'),
+    path('rider/', views.rider, name='rider'),
+    path('settings/', views.settings, name='settings'),
 ]
--- adminpages/views.py
+++ adminpages/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'adminpages/home.html')
+
+def dashboard(request):
+    return render(request,'adminpages/dashboard.html')
+
+def booking(request):
+    return render(request,'adminpages/booking.html')
+
+def payment(request):
+    return render(request,'adminpages/payment.html')
+
+def report(request):
+    return render(request,'adminpages/report.html')
+
+def rider(request):
+    return render(request,'adminpages/rider.html')
+
+def settings(request):
+    return render(request,'adminpages/settings.html')
