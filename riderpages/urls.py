638d982971d1cbe0662565b10b0ffbf56a2f0a9f
joelscariajustine123 <joelscaria2002@gmail.com>
2025-07-22 21:24:55 +0530

    add riderpages

--- riderpages/urls.py
+++ riderpages/urls.py
@@ -3,4 +3,8 @@ from .views import *
 app_name = 'riderpages'
 urlpatterns = [
     path('',index,name='index'),
+    path('safety/',safety,name='safety'),
+    path('outstation/',outstation,name='outstation'),
+    path('contact/',contact,name='contact'),
+    path('services/',services,name='services'),
 ]
--- riderpages/views.py
+++ riderpages/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'riderpages/index.html')
+def safety(request):
+    return render(request,'riderpages/Safety.html')
+def outstation(request):
+    return render(request,'riderpages/outstation.html')
+def contact(request):
+    return render(request,'riderpages/contact.html')
+def services(request):
+    return render(request,'riderpages/services.html')
